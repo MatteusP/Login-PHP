847a2847068869ebef98870c5f95fdace491474e
Mateus Paiva <matteusp@alu.ufc.br>
2024-10-29 10:39:38 -0300

    Conexão com banco de dados

---
+++ includes/db.php
@@ -0,0 +1,14 @@
+<?php
+// includes/db.php
+
+$host = 'localhost';
+$dbname = 'test';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro de conexão: " . $e->getMessage());
+}
